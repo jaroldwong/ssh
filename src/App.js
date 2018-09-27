901b1a0b4ae6ae85404ef3c296ee684b0cf6be11
Jarold Wong <jaroldw@gmail.com>
2018-09-27 15:58:27 -0700

    Test reading xlsx data

--- src/App.js
+++ src/App.js
@@ -5,7 +5,27 @@ import CalendarGrid from './components/CalendarGrid';
 import SummaryCard from './components/SummaryCard';
 import { Grid } from '@material-ui/core';
 
+import axios from 'axios';
+import XLSX from 'xlsx';
+
 class App extends Component {
+  componentDidMount() {
+    let URL = process.env.REACT_APP_URL;
+    let TOKEN = process.env.REACT_APP_TOKEN;
+
+    debugger;
+    axios(URL, {
+      headers: { Authorization: `Bearer ${TOKEN}` },
+      responseType: 'arraybuffer'
+    }).then(res => {
+      let data = new Uint8Array(res.data);
+      let wb = XLSX.read(data, { type: 'array' });
+
+      let JSON = XLSX.utils.sheet_to_json(wb.Sheets[wb.SheetNames[0]]);
+      console.log(JSON);
+    });
+  }
+
   render() {
     return (
       <div>
